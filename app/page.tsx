fb2f7a0c388e0d87d3c247f07849a75461bc5f42
Leonardo Omaña <leotangram@gmail.com>
2023-07-15 22:18:47 -0500

    Go to about page from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import Link from 'next/link'
+
+export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <span className="text-5xl">Hola mundo!</span>
+
+      <Link href="/about">About Page</Link>
     </main>
   )
 }
